91824e2208a58226e6797e158b56ebda5291a86a
Xbetty <1623290520@qq.com>
2017-12-06 22:16:18 +0800

    修改身份号的验证条件

--- WebContent/js/Technology/ContrastFingerPrint/ContrastFingerPrintGetBirth.js
+++ WebContent/js/Technology/ContrastFingerPrint/ContrastFingerPrintGetBirth.js
@@ -2,7 +2,7 @@ function ContrastFingerPrintGetBirth(){
 	var input_suspecter_identity=document.getElementById("input_suspecter_identity");
 	var input_suspecter_identityValue=input_suspecter_identity.value.trim();
 	var input_suspecter_birthday=document.getElementById("input_suspecter_birthday");
-	var strRegExp=/(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
+	var strRegExp=/(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
 		if(strRegExp.test(input_suspecter_identityValue)==true){
 			input_suspecter_birthday.value=input_suspecter_identityValue.substr(6,4)+"-"+input_suspecter_identityValue.substr(10,2)+"-"+input_suspecter_identityValue.substr(12,2);
 		} 
