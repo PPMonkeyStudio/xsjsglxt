dd3fd551142b681ba96195e6c4d3189722c19131
LiWenKaigit <18779573765@163.com>
2017-12-20 11:51:30 +0800

    aaa

--- test/xsjsglxt/Casetest.java
+++ test/xsjsglxt/Casetest.java
@@ -1,43 +1,33 @@
 package xsjsglxt;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.annotation.Resource;
 
-import org.hibernate.SessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.xsjsglxt.dao.Case.SenceDao;
-import com.xsjsglxt.domain.DO.xsjsglxt_briefdetails;
-import com.xsjsglxt.domain.DO.xsjsglxt_case;
-import com.xsjsglxt.domain.DO.xsjsglxt_parallel;
-import com.xsjsglxt.domain.DO.xsjsglxt_staff;
-import com.xsjsglxt.domain.VO.Case.page_list_CasematerialVO;
-import com.xsjsglxt.domain.VO.Case.page_list_senceInformationVO;
-import com.xsjsglxt.domain.VO.Team.page_list_staffInformationVO;
-import com.xsjsglxt.service.Case.ImageService;
-import com.xsjsglxt.service.Case.LostService;
-import com.xsjsglxt.service.Case.ParallelService;
-import com.xsjsglxt.service.Case.SenceService;
-import com.xsjsglxt.service.Team.StaffService;
+import com.xsjsglxt.domain.VO.Case.page_list_HandleInformationVO;
+import com.xsjsglxt.service.Case.HandleService;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:applicationContext*.xml" })
 public class Casetest {
 @Resource
-private StaffService staffService;
+private HandleService handleService;
 
-public void setStaffService(StaffService staffService) {
-	this.staffService = staffService;
+
+public void setHandleService(HandleService handleService) {
+	this.handleService = handleService;
 }
+
+
 @Test
 public void s(){
-	xsjsglxt_staff staff=new xsjsglxt_staff();
-	staff.setStaff_age("18");
-	staffService.saveStaff(staff);
+	page_list_HandleInformationVO page_list_HandleInformation=new page_list_HandleInformationVO();
+	page_list_HandleInformation.setHandle_administrativeCase("1");
+	handleService.VO_HandleInformation_By_PageAndSearch(page_list_HandleInformation);
 	
 }
+
 }
