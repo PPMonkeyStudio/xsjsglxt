23bb20a80f14c03717576b35a23e385e05a6ec3f
sunyi970505 <1059747381@qq.com>
2018-01-18 13:12:08 +0800

    增加 ，修改，删除会议记录书

--- src/com/xsjsglxt/dao/User/MeetingDao.java
+++ src/com/xsjsglxt/dao/User/MeetingDao.java
@@ -1,5 +1,12 @@
 package com.xsjsglxt.dao.User;
 
+import com.xsjsglxt.domain.DO.xsjsglxt_meeting;
+
 public interface MeetingDao {
+	public String saveMeetingRecords(xsjsglxt_meeting meet);
+
+	public String updateMeetingRecords(xsjsglxt_meeting meet);
+
+	public String deleteMeetingRecords(String[] meeting_id);
 
 }
--- src/com/xsjsglxt/dao/impl/User/MeetingDaoImpl.java
+++ src/com/xsjsglxt/dao/impl/User/MeetingDaoImpl.java
@@ -1,9 +1,11 @@
 package com.xsjsglxt.dao.impl.User;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import com.xsjsglxt.dao.User.MeetingDao;
+import com.xsjsglxt.domain.DO.xsjsglxt_meeting;
 
 public class MeetingDaoImpl implements MeetingDao {
 	private SessionFactory sessionFactory;
@@ -20,4 +22,58 @@ public class MeetingDaoImpl implements MeetingDao {
 		this.sessionFactory = sessionFactory;
 	}
 
+	// 保存会议记录书
+
+	@Override
+	public String saveMeetingRecords(xsjsglxt_meeting meet) {
+		// TODO Auto-generated method stub
+
+		try {
+			Session session = this.getSession();
+			session.saveOrUpdate(meet);
+			session.clear();
+		} catch (Exception e) {
+			// TODO: handle exception
+			return "saveFail";
+		}
+
+		return "savueSuccess";
+	}
+
+	// 修改会议记录书
+	@Override
+	public String updateMeetingRecords(xsjsglxt_meeting meet) {
+		// TODO Auto-generated method stub
+		try {
+			Session session = this.getSession();
+			session.saveOrUpdate(meet);
+			session.clear();
+		} catch (Exception e) {
+			// TODO: handle exception
+			return "updateFail";
+		}
+
+		return "updateSuccess";
+	}
+
+	// 批量删除会议记录书
+
+	@Override
+	public String deleteMeetingRecords(String[] meeting_id) {
+		// TODO Auto-generated method stub
+		Session session = this.getSession();
+		try {
+
+			for (int i = 0; i < meeting_id.length; i++) {
+				String hql = "delete xsjsglxt_meeting where meeting_id = '" + meeting_id[i] + "'";
+				Query query = session.createQuery(hql);
+				query.executeUpdate();
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			return "deleteFail";
+		}
+
+		return "deleteSuccess";
+	}
 }
