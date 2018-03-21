046d93ebd034576b10fb996313c1deea162903e0
sunyi970505 <1059747381@qq.com>
2018-03-21 16:25:37 +0800

    学习经历完成

--- src/com/xsjsglxt/dao/Team/StaffStudentDao.java
+++ src/com/xsjsglxt/dao/Team/StaffStudentDao.java
@@ -1,5 +1,19 @@
 package com.xsjsglxt.dao.Team;
 
+import java.util.List;
+
+import com.xsjsglxt.domain.DO.xsjsglxt_staffStudent;
+
 public interface StaffStudentDao {
+
+	String saveStudents(List<xsjsglxt_staffStudent> students);
+
+	String deleteStudent(String xsjsglxt_staffStudent_id);
+
+	String deleteStudents(String staffStudent_staff);
+
+	xsjsglxt_staffStudent getStudent(String xsjsglxt_staffStudent_id);
+
+	List<xsjsglxt_staffStudent> getStudents(String staffStudent_staff);
 
 }
--- src/com/xsjsglxt/service/impl/Team/StaffStudentServiceImpl.java
+++ src/com/xsjsglxt/service/impl/Team/StaffStudentServiceImpl.java
@@ -1,7 +1,12 @@
 package com.xsjsglxt.service.impl.Team;
 
+import java.util.List;
+
 import com.xsjsglxt.dao.Team.StaffStudentDao;
+import com.xsjsglxt.domain.DO.xsjsglxt_staffStudent;
 import com.xsjsglxt.service.Team.StaffStudentService;
+
+import util.TeamUtil;
 
 public class StaffStudentServiceImpl implements StaffStudentService {
 	private StaffStudentDao staffStudentDao;
@@ -12,5 +17,55 @@ public class StaffStudentServiceImpl implements StaffStudentService {
 
 	public void setStaffStudentDao(StaffStudentDao staffStudentDao) {
 		this.staffStudentDao = staffStudentDao;
+	}
+
+	@Override
+	public String saveStudents(List<xsjsglxt_staffStudent> students) {
+		// TODO Auto-generated method stub
+		for (xsjsglxt_staffStudent xsjsglxt_staffStudent : students) {
+			xsjsglxt_staffStudent.setXsjsglxt_staffStudent_id(TeamUtil.getUuid());
+			xsjsglxt_staffStudent.setStaffStudent_gmt_create(TeamUtil.getStringSecond());
+			xsjsglxt_staffStudent.setStaffStudent_gmt_modified(TeamUtil.getStringSecond());
+		}
+		String result = staffStudentDao.saveStudents(students);
+		return result;
+	}
+
+	@Override
+	public String deleteStudent(String xsjsglxt_staffStudent_id) {
+		// TODO Auto-generated method stub
+		String result = staffStudentDao.deleteStudent(xsjsglxt_staffStudent_id);
+		return result;
+	}
+
+	@Override
+	public String deleteStudents(String staffStudent_staff) {
+		// TODO Auto-generated method stub
+		String result = staffStudentDao.deleteStudents(staffStudent_staff);
+		return result;
+	}
+
+	@Override
+	public String upadteStudent(xsjsglxt_staffStudent student) {
+		// TODO Auto-generated method stub
+		xsjsglxt_staffStudent oldStudent = staffStudentDao.getStudent(student.getXsjsglxt_staffStudent_id());
+		oldStudent.setStaffStudent_gmt_modified(TeamUtil.getStringSecond());
+		oldStudent.setStaffStudent_remarks(student.getStaffStudent_remarks());
+		oldStudent.setStaffStudent_address(student.getStaffStudent_address());
+		oldStudent.setStaffStudent_startTime(student.getStaffStudent_startTime());
+		oldStudent.setStaffStudent_stopTime(student.getStaffStudent_stopTime());
+		return "updateSuccess";
+	}
+
+	@Override
+	public xsjsglxt_staffStudent getStudent(String xsjsglxt_staffStudent_id) {
+		// TODO Auto-generated method stub
+		return staffStudentDao.getStudent(xsjsglxt_staffStudent_id);
+	}
+
+	@Override
+	public List<xsjsglxt_staffStudent> getStudents(String staffStudent_staff) {
+		// TODO Auto-generated method stub
+		return staffStudentDao.getStudents(staffStudent_staff);
 	}
 }
